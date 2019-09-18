de65ed4781bc0460b08ac888152e615419a06727
Merlin <merlin.p.varghese@gmail.com>
2019-09-18 13:32:38 +0530

    Initial commit

---
+++ src/test/java/com/example/product/ProductApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.product;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ProductApplicationTests {
+	@Test
+	public void expectRunningTests() {
+		assertTrue(true);
+	}
+}
